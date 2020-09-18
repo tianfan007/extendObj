93bc898fa4bfe31683c5b1586a2ea8b519029289
tianfan <arrbo@163.com>
2020-09-18 16:07:51 +0800

    Update extendObj.php
    add more method and example

--- extendObj.php
+++ extendObj.php
@@ -34,26 +34,23 @@ class stringObj {
 		return new intObj(\strlen($this->_string));
 	}
 	
-	public function md5():stringObj {
-		return new stringObj(\md5($this->_string));
+	public function md5(bool $output=false):stringObj {
+		return new stringObj(\md5($this->_string,$output));
 	}
 	
 	public function substr():stringObj {
+		
 		$params= \func_get_args();
-		return \array_key_exists(1, $params)?new stringObj(\substr($this->_string, $params[0],  $params[1])) : new stringObj(\substr($this->_string, $params[0]));
+		return new stringObj(\substr($this->_string, ...$params));
+	}
+	
+	public function sha1(bool $ouput=false):stringObj {
+		return new stringObj(\sha1($this->_string,$output));
 	}
 	
 	public function chunkSplit():stringObj{
-		$n= \func_num_args();
 		$params= \func_get_args();
-		switch($n){
-			case 0:
-				return new stringObj(chunk_split($this->_string));
-			case 1:
-				return new stringObj(chunk_split($this->_string,$params[0]));
-			case 2:
-				return new stringObj(chunk_split($this->_string,$params[0], $params[1]));
-		}
+		return new stringObj(chunk_split($this->_string,...$params));
 	}
 	
 	public function crc32():intObj{
@@ -62,7 +59,7 @@ class stringObj {
 	
 	public function crypt():stringObj{
 		$params= \func_get_args();
-		return \array_key_exists(0, $params)?new stringObj(\crypt ($this->_string, $params[0])) : new stringObj(\crypt ($this->_string));
+		return new stringObj(\crypt ($this->_string, ...$params));
 	}
 	
 	public function echo():void {
@@ -76,6 +73,21 @@ class stringObj {
 	public function rtrim():stringObj {
 		//todo
 	}
+        public function trim():stringObj {
+		//todo
+	}
+        public function ord():intObj{
+            return new intObj(ord($this->_string));
+        }
+        public function replace(string $repacement,string $replace,int $count=1):stringObj{
+            return new stringObj(str_replace($repacement, $replace, $this->_string,$count));
+        }
+        public function isEmpty():bool{
+            return empty($this->_array);
+        }
+        public function match(string $regexp):bool{
+            return preg_match($regexp, $this->_string);
+        }
 }
 
 
@@ -90,6 +102,9 @@ class intObj {
 	public function chr():stringObj{
 		return new stringObj(\chr($this->_int));
 	}
+        public function toString():stringObj{
+            return new stringObj(strval($this->_int));
+        }
 }
 
 
@@ -113,16 +128,56 @@ class arrayObj{
 	public function implode(string $glue):stringObj{
 		return new stringObj(implode($glue,$this->_array));
 	}
+        public function replace(array $repacement,array $replace,int $count=1):arrayObj{
+            return new arrayObj(str_replace($repacement, $replace, $this->_string,$count));
+        }
+        public function sort(int $sort_flags=SORT_REGULAR):arrayObj{
+            \sort($this->_array,$sort_flags);
+            return $this;
+        }
+        public function rsort(int $sort_flags=SORT_REGULAR):arrayObj{
+            \rsort($this->_array,$sort_flags);
+            return $this;
+        }
+        public function asort(int $sort_flags=SORT_REGULAR):arrayObj{
+            \asort($this->_array,$sort_flags);
+            return $this;
+        }
+        public function arsort(int $sort_flags=SORT_REGULAR):arrayObj{
+            \arsort($this->_array,$sort_flags);
+            return $this;
+        }
+        public function length():intObj{
+            return new intObj(count($this->_array));
+        }
+        public function pop():arrayObj{
+            \array_pop($this->_array);
+            return $this;
+        }
+        public function push():arrayObj{
+            \array_push($this->_array);
+            return $this;
+        }
+        public function shift():arrayObj{
+            \array_shift($this->_array);
+            return $this;
+        }
 }
+
 #################################example#############################################
 $a=new stringObj('abcdefg');
 $b=$a->md5();
 echo $b."\n";
-$c= $b->substr(10,3);
+$c= $b->substr(3);
 echo $c."\n";
 $d=new intObj(40);
 echo $d->chr()."\n";
-$array=new arrayObj([5,7,9]);
-$array->each(function($key,$value){
-	echo $value." \n";
+$array=new arrayObj([5,7,9,6,8,10,3]);
+$array->rsort()->each(function($key,$value){
+	echo $key."=>".$value." \n";
 });
+$array->pop()->each(
+        function($key,$value){
+	echo $key."=>".$value." \n";
+}
+        );
